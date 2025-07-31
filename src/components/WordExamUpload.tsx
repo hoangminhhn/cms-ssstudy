8821fb99b5bb3a4dd5920a7d8afc744d12f0f30b
[dyad] <hoangminhphungbk@gmail.com>
2025-07-31 17:20:16 +0700

    [dyad] wrote 1 file(s)

--- src/components/WordExamUpload.tsx
+++ src/components/WordExamUpload.tsx
@@ -250,7 +250,7 @@ const WordExamUpload: React.FC = () => {
           </div>
 
           {/* Second row */}
-          <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-8 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-8 gap-4">
             <div>
               <Label htmlFor="exam-group">Nhóm đề</Label>
               <Select
@@ -312,8 +312,8 @@ const WordExamUpload: React.FC = () => {
             </div>
           </div>
 
-          {/* Third row: Thành phố */}
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {/* Third row */}
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 pt-2">
             <div>
               <Label htmlFor="city-select">Thành phố</Label>
               <Select
