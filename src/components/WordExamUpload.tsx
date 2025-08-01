dffcd6a2ef1bd319282b4e89959c9f7478239366
[dyad] <hoangminhphungbk@gmail.com>
2025-08-01 14:46:34 +0700

    [dyad] wrote 1 file(s)

--- src/components/WordExamUpload.tsx
+++ src/components/WordExamUpload.tsx
@@ -235,17 +235,17 @@ const WordExamUpload: React.FC = () => {
         <CardHeader>
           <CardTitle>Thông tin đề thi</CardTitle>
         </CardHeader>
-        <CardContent className="grid grid-cols-8 gap-4">
+        <CardContent className="grid grid-cols-16 gap-4">
           {/* Row 1 */}
-          <div className="col-span-1">
+          <div className="col-span-2">
             <Label htmlFor="exam-code">Mã đề thi</Label>
             <Input id="exam-code" value={examCode} disabled />
           </div>
-          <div className="col-span-3">
+          <div className="col-span-6">
             <Label htmlFor="exam-name">Tên đề thi</Label>
             <Input id="exam-name" value={examName} onChange={(e) => setExamName(e.target.value)} placeholder="Nhập tên đề thi" />
           </div>
-          <div className="col-span-2">
+          <div className="col-span-4">
             <Label htmlFor="exam-period">Kỳ thi</Label>
             <Select value={examPeriod} onValueChange={setExamPeriod}>
               <SelectTrigger id="exam-period">
@@ -258,7 +258,7 @@ const WordExamUpload: React.FC = () => {
               </SelectContent>
             </Select>
           </div>
-          <div className="col-span-2">
+          <div className="col-span-4">
             <Label htmlFor="part">Phần thi</Label>
             <Select value={part} onValueChange={setPart}>
               <SelectTrigger id="part">
@@ -273,7 +273,7 @@ const WordExamUpload: React.FC = () => {
           </div>
 
           {/* Row 2 */}
-          <div className="col-span-1">
+          <div className="col-span-2">
             <Label htmlFor="test-type">Loại bài kiểm tra</Label>
             <Select value={testType} onValueChange={setTestType}>
               <SelectTrigger id="test-type">
@@ -286,7 +286,7 @@ const WordExamUpload: React.FC = () => {
               </SelectContent>
             </Select>
           </div>
-          <div className="col-span-1">
+          <div className="col-span-2">
             <Label htmlFor="group">Nhóm đề</Label>
             <Select value={group} onValueChange={setGroup}>
               <SelectTrigger id="group">
@@ -298,7 +298,7 @@ const WordExamUpload: React.FC = () => {
               </SelectContent>
             </Select>
           </div>
-          <div className="col-span-1">
+          <div className="col-span-2">
             <Label htmlFor="class-level">Lớp</Label>
             <Select value={classLevel} onValueChange={setClassLevel}>
               <SelectTrigger id="class-level">
@@ -337,7 +337,7 @@ const WordExamUpload: React.FC = () => {
               </SelectContent>
             </Select>
           </div>
-          <div className="col-span-2">
+          <div className="col-span-4">
             <Label htmlFor="city">Thành phố</Label>
             <Select value={city} onValueChange={setCity}>
               <SelectTrigger id="city">
