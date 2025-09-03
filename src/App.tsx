84b5b48484be62c4fc34cea96a0f8066588eb563
[dyad] <hoangminhphungbk@gmail.com>
2025-09-03 22:24:04 +0700

    Reverted all changes back to version 35242052b95d84ce145a6d552f6b6e05cc4f1592

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import BookManagement from "@/pages/BookManagement";
-import ExamManagement from "@/pages/ExamManagement";
-import WordExamUploadPage from "@/pages/WordExamUploadPage";
-import ScoreReportPage from "@/pages/ScoreReportPage";
-import CourseManagement from "@/pages/CourseManagement";
-import LessonManagement from "@/pages/LessonManagement";
-import QuizManagement from "@/pages/QuizManagement";
-import DocumentManagement from "@/pages/DocumentManagement";
-import MemberManagement from "@/pages/MemberManagement";
-import OrderManagement from "@/pages/OrderManagement";
-import PromotionManagement from "@/pages/PromotionManagement";
-import NewsPage from "@/pages/NewsPage";
-import NotificationManagement from "@/pages/NotificationManagement";
 
 const queryClient = new QueryClient();
 
@@ -29,19 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/books" element={<BookManagement />} />
-          <Route path="/exams" element={<ExamManagement />} />
-          <Route path="/word-exam-upload" element={<WordExamUploadPage />} />
-          <Route path="/score-reports/:examId" element={<ScoreReportPage />} />
-          <Route path="/courses" element={<CourseManagement />} />
-          <Route path="/lessons" element={<LessonManagement />} />
-          <Route path="/quizzes" element={<QuizManagement />} />
-          <Route path="/documents" element={<DocumentManagement />} />
-          <Route path="/members" element={<MemberManagement />} />
-          <Route path="/orders" element={<OrderManagement />} />
-          <Route path="/promotions" element={<PromotionManagement />} />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/notifications" element={<NotificationManagement />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
