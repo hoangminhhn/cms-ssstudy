e4b0027be4652bc6dc77e6e2612c25f8cd84bb8f
[dyad] <hoangminhphungbk@gmail.com>
2025-09-05 15:58:17 +0700

    [dyad] Fixed Button import and isolated OtherInfo - wrote 1 file(s)

--- src/components/courses/OtherInfo.tsx
+++ src/components/courses/OtherInfo.tsx
@@ -7,6 +7,7 @@ import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
 import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
 
 const OtherInfo: React.FC = () => {
   const [studyMode, setStudyMode] = React.useState<"Offline" | "Online">("Offline");
